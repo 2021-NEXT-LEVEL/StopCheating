4c6a8a596dfd80b86c91a59ebb3996241853b150
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-06-01 18:55:39 +0900

    ADD detect-peoples

--- main.py
+++ main.py
@@ -1,74 +1,104 @@
-# 패키지 설치
-# pip install dlib opencv-python
-#
-# 학습 모델 다운로드
-# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 import dlib
 import cv2 as cv
 import numpy as np
+from tkinter import *
+import tkinter.messagebox
+from datetime import datetime
 
-detector = dlib.get_frontal_face_detector()
 
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+def askStartExam():  # [시험 시작] 프로그램 실행 시 시작 여부를 사용자에게 물어봄
+    MsgBox = tkinter.messagebox.askquestion("Message", "시험을 시작하시겠습니까?")
+    if MsgBox == 'yes':
+        startExam()
+    else:
+        return
 
-cap = cv.VideoCapture(0)
 
-# range는 끝값이 포함안됨
-ALL = list(range(0, 68))
-RIGHT_EYEBROW = list(range(17, 22))
-LEFT_EYEBROW = list(range(22, 27))
-RIGHT_EYE = list(range(36, 42))
-LEFT_EYE = list(range(42, 48))
-NOSE = list(range(27, 36))
-MOUTH_OUTLINE = list(range(48, 61))
-MOUTH_INNER = list(range(61, 68))
-JAWLINE = list(range(0, 17))
+def askFinishExam():  # [시험 종료] 시험을 종료시킴
+    MsgBox = tkinter.messagebox.askquestion("Message", "시험을 종료하시겠습니까?")
+    if MsgBox == 'yes':
+        window.destroy()
 
-index = ALL
 
-while True:
+def alert(case):  # 부정행위 감지 시 alert
+    if case == 1:
+        tkinter.messagebox.showinfo("Alert", "두 명 이상 감지되었습니다.")
+    now = datetime.now()
+    print("alert log : %s년 %s월 %s일 %s시 %s분 %s초.%s" % (now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond))
 
-    ret, img_frame = cap.read()
 
-    img_gray = cv.cvtColor(img_frame, cv.COLOR_BGR2GRAY)
+def startExam():  # [시험 시작] 버튼 클릭 시 부정행위 감지 프로그램 시작
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    cap = cv.VideoCapture(0)
 
-    dets = detector(img_gray, 1)
+    ALL = list(range(0, 68))
+    RIGHT_EYEBROW = list(range(17, 22))
+    LEFT_EYEBROW = list(range(22, 27))
+    RIGHT_EYE = list(range(36, 42))
+    LEFT_EYE = list(range(42, 48))
+    NOSE = list(range(27, 36))
+    MOUTH_OUTLINE = list(range(48, 61))
+    MOUTH_INNER = list(range(61, 68))
+    JAWLINE = list(range(0, 17))
 
-    for face in dets:
+    index = ALL
 
-        shape = predictor(img_frame, face)  # 얼굴에서 68개 점 찾기
+    while True:
+        ret, img_frame = cap.read()
+        img_gray = cv.cvtColor(img_frame, cv.COLOR_BGR2GRAY)
+        dets = detector(img_gray, 1)
 
-        list_points = []
-        for p in shape.parts():
-            list_points.append([p.x, p.y])
+        for face in dets:
+            if len(dets) > 1: # case == 1 -> 두 명 이상 감지
+                alert(1)
+            shape = predictor(img_frame, face)  # 얼굴에서 68개 점 찾기
+            list_points = []
+            for p in shape.parts():
+                list_points.append([p.x, p.y])
 
-        list_points = np.array(list_points)
+            list_points = np.array(list_points)
 
-        for i, pt in enumerate(list_points[index]):
-            pt_pos = (pt[0], pt[1])
-            cv.circle(img_frame, pt_pos, 2, (0, 255, 0), -1)
+            for i, pt in enumerate(list_points[index]):
+                pt_pos = (pt[0], pt[1])
+                cv.circle(img_frame, pt_pos, 2, (0, 255, 0), -1)
 
-        cv.rectangle(img_frame, (face.left(), face.top()), (face.right(), face.bottom()),
-                     (0, 0, 255), 3)
+            cv.rectangle(img_frame, (face.left(), face.top()), (face.right(), face.bottom()),
+                         (0, 0, 255), 3)
+        key = cv.waitKey(1)
 
-    cv.imshow('result', img_frame)
+        cv.imshow('result', img_frame)
 
-    key = cv.waitKey(1)
+        if key == 27:
+            break
+        elif key == ord('1'):
+            index = ALL
+        elif key == ord('2'):
+            index = LEFT_EYEBROW + RIGHT_EYEBROW
+        elif key == ord('3'):
+            index = LEFT_EYE + RIGHT_EYE
+        elif key == ord('4'):
+            index = NOSE
+        elif key == ord('5'):
+            index = MOUTH_OUTLINE + MOUTH_INNER
+        elif key == ord('6'):
+            index = JAWLINE
 
-    if key == 27:
-        break
+    cap.release()
 
-    elif key == ord('1'):
-        index = ALL
-    elif key == ord('2'):
-        index = LEFT_EYEBROW + RIGHT_EYEBROW
-    elif key == ord('3'):
-        index = LEFT_EYE + RIGHT_EYE
-    elif key == ord('4'):
-        index = NOSE
-    elif key == ord('5'):
-        index = MOUTH_OUTLINE + MOUTH_INNER
-    elif key == ord('6'):
-        index = JAWLINE
 
-cap.release()
+if __name__ == "__main__":
+    window = Tk()
+    window.title("온라인 시험 부정행위 방지 프로그램")
+
+    base_frame = Frame(window)
+    base_frame.pack()
+
+    btn_start_exam = Button(base_frame, text="시험 시작", width="20", command=askStartExam)
+    btn_start_exam.pack(pady="10")
+
+    btn_finish_exam = Button(base_frame, text="시험 종료", width="20", command=askFinishExam)
+    btn_finish_exam.pack(pady="10")
+
+    window.mainloop()
+    window.after(100000, askFinishExam) # 시험시간 : 1분 후 종료
